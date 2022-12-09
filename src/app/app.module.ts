d33129ff0625a29c13b580fad5bf4e9d0fa3905b
Carolista <cjones.renaissancewoman@gmail.com>
2022-12-08 20:19:19 -0600

    added experiments and equipment components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CrewComponent } from './crew/crew.component';
+import { EquipmentComponent } from './equipment/equipment.component';
+import { ExperimentsComponent } from './experiments/experiments.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CrewComponent,
+    EquipmentComponent,
+    ExperimentsComponent
   ],
   imports: [
     BrowserModule
